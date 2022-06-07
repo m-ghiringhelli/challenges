dbe95b2f4c6ff8f81bdcadb0af900c2d32c74be2
Cole Rossman <colerossman99@gmail.com>
2022-06-07 16:02:51 -0700

    attempt at testing que class

--- index.js
+++ index.js
@@ -36,19 +36,6 @@ class Queue {
     return (this.#list.length === 0) ? false : true;
   }
 }
-const queue = new Queue();
-queue.enqueue('fox');
-queue.enqueue('goose');
-queue.enqueue('lizard');
-console.log(queue.dequeue()); // 'fox'
-console.log(queue.hasNext()); // true
-console.log(queue.dequeue()); // 'goose'
-queue.enqueue('llama');
-console.log(queue.dequeue()); // 'lizard'
-console.log(queue.hasNext()); // true
-console.log(queue.dequeue()); // 'llama'
-console.log(queue.hasNext()); // false
-console.log(queue.dequeue()); // null
 module.exports = { Stack, Queue };
 
 
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { Stack } = require("./index.js");
+const { Stack, Queue } = require("./index.js");
 describe("Stack Class", () => {
   it("#push should add a new item to the stack ", () => {
     const stack = new Stack();
@@ -13,4 +13,20 @@ describe("Stack Class", () => {
     stack.pop();
     expect(stack.peek()).toEqual(1);
   })
+});
+
+describe("Que Class", () => {
+  it("enqueue should add a new item to the queue ", () => {
+    const queue = new Queue();
+    queue.enqueue(1);
+    expect(queue[0]).toEqual(1);
+  });
+  // add more tests here...
+  // it('#pop should return the last item in the stack', () => {
+  //   const stack = new Queue();
+  //   stack.push(1);
+  //   stack.push(2);
+  //   stack.pop();
+  //   expect(stack.peek()).toEqual(1);
+  // })
 });
