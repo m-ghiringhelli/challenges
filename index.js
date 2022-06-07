21e15675392c93816967b9c4f1ea64bc57239ab7
Cole Rossman <colerossman99@gmail.com>
2022-06-07 15:52:55 -0700

    class que returns proper data

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ class Stack {
   pop(item) {
     const poppedItem = this.#list.pop(item);
 
-    if (!poppedItem) {
-      return null;
-    } else {
-      return poppedItem;
-    }
+    return !poppedItem ? null : poppedItem;
   }
   
   peek(item) {
@@ -21,19 +17,39 @@ class Stack {
   }
 }
 
-const stack = new Stack();
-stack.push('fox');
-stack.push('goose');
-stack.push('lizard');
-console.log(stack.pop()); // 'lizard'
-console.log(stack.peek()); // 'goose'
-console.log(stack.pop()); // 'goose'
-stack.push('llama');
-console.log(stack.pop()); // 'llama'
-console.log(stack.peek()); // 'fox'
-console.log(stack.pop()); // 'fox'
-console.log(stack.pop()); // null
+class Queue {
+  #list = [];
 
-class Queue {}
+  enqueue(item) {
+    this.#list.push(item);
+  }
 
+  dequeue() {
+    const dequeuedItem = this.#list.shift();
+
+    return !dequeuedItem ? null : dequeuedItem;
+  }
+
+
+  hasNext() {
+   
+    return (this.#list.length === 0) ? false : true;
+  }
+}
+const queue = new Queue();
+queue.enqueue('fox');
+queue.enqueue('goose');
+queue.enqueue('lizard');
+console.log(queue.dequeue()); // 'fox'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'goose'
+queue.enqueue('llama');
+console.log(queue.dequeue()); // 'lizard'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'llama'
+console.log(queue.hasNext()); // false
+console.log(queue.dequeue()); // null
 module.exports = { Stack, Queue };
+
+
+
